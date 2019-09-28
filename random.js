b1a662eb84df2db1c371bef194c035b8130d5cb5
Chengwei Ye <chengweiye@Chengweis-MacBook-Pro.local>
2019-09-28 16:53:34 -0400

    delete luck

--- random.js
+++ random.js
@@ -1,4 +1,12 @@
 let flag = 1;
+const from = 449001;
+const to = 449200;
+let randomIndex = 0;
+
+let arr = []
+for(let i=from;i<=to;i++){
+    arr.push(i);
+}
 function start(){
     flag = 1;
     changeNumber();
@@ -8,11 +16,13 @@ function changeNumber(){
     if(flag==0){
         return
     }
+    randomIndex = d3.randomInt(0, to-from+1)();
     d3.select('#num')
-      .text(d3.randomInt(0, 159)());
-    setTimeout("changeNumber()",50);
+      .text(arr[randomIndex]);
+    setTimeout('changeNumber()',50);
 }
 
 function stop(){
     flag = 0
+    arr.splice(randomIndex,1);
 }
